643a3181553be0babf3894251a3a3dfc7de0e492
Akshat <akshat41121995@gmail.com>
2022-05-17 20:59:38 +0200

    Tests on LR working fine...

--- code/Internals/Cost.py
+++ code/Internals/Cost.py
@@ -1,12 +1,16 @@
 import numpy as np
 
-
 class Cost:
     def __init__(self) -> None:
         pass
     
+    def Hypothesis(self, X, theta):
+        self.X = X
+        self.theta = theta
+        return np.matmul(self.X, self.theta)
+
     def LinearRegressionCost(self,x,y, theta):
-        return ((hypothesis(x,theta) - y).T@(hypothesis(x,theta)-y)/(2*y.shape[0])) 
+        return ((self.Hypothesis(x,theta) - y).T@(self.Hypothesis(x,theta)-y)/(2*y.shape[0])) 
         
     def LogisticRegressionCost(self):
         return None
--- code/Regression/LinearRegression.py
+++ code/Regression/LinearRegression.py
@@ -6,75 +6,59 @@ import numpy as np
 from Internals.GradientDescent import GradientDescent
 from Internals.Cost import Cost
 
-class LinearRegression:
+class LinearRegression():
     def __init__(self) -> None:
         self.beta = None
         self.mu = []
         self.std = []
+        self.c = Cost()
         pass
     
     def fit(self, X,Y):
         
-
-
         self.X = X
         self.Y = Y
         
         self.mu.append(np.mean(self.X, axis=1))
         self.std.append(np.std(self.X, axis=1))
-        print("Mean is: ", self.mu)
-        print("std is: ", self.std)
         self.X = self.X.T
-
-        self.Y = np.reshape(self.Y, (4,1))
         self.X = np.hstack((np.ones((self.X.shape[0],1)), self.X))
-        
-        print("Input X Shape: ", self.X.shape)
-        print("Input Y Shape: ", self.Y.shape)
-        
+        self.Y = np.reshape(self.Y, (10,1))
         theta = np.zeros((self.X.shape[1],1))
-        print("Theta Shape: ",theta.shape)
-
-        def hypothesis(X, theta):
-                return np.matmul(X,theta)
-            
-        def cost(x,y, theta):
-            return ((hypothesis(x,theta) - y).T@(hypothesis(x,theta)-y))/(2*y.shape[0]) 
         
-        def gradient_descent(x,y, theta, learning_rate = 0.1, num_epocs = 10):
+        def gradient_descent(x,y, theta, learning_rate = 0.1, num_epocs = 500):
             m = x.shape[0]
             J_all = []
 
             for _ in range(num_epocs):
-                h_x = hypothesis(x, theta)
+                h_x = self.c.Hypothesis(x, theta)
                 cost_ = (1/m)*(x.T@(h_x - y))
                 theta = theta - (learning_rate)*cost_
-                J_all.append(cost(x, y, theta))
+                J_all.append(self.c.LinearRegressionCost(x,y, theta))
             
             return theta, J_all
         
-        theta, J_all = gradient_descent(self.X, self.Y, theta, learning_rate = 0.1, num_epocs = 50)
+        theta, J_all = gradient_descent(self.X, self.Y, theta, learning_rate = 0.01, num_epocs = 50)
         self.beta = theta
         print("Theta Shape After GD", theta.shape)
         print(self.beta)
         
 
     def predict(self, X):
-
         self.X = X
-        self.X[0] = (self.X[0] - self.mu[0])/self.std[0]
+        # self.X[0] = (self.X[0] - self.mu[0])/self.std[0]
         y_pred = self.beta[0] + self.beta[1]*self.X[0] 
         # print(self.beta)
         print("Prediction is: ", y_pred)
         return y_pred
     
         
-X = np.array([[1,2,3,4]])
-Y = np.array([[10,20,30,40]])
+X = np.array([[1,2,3,4,5,6,7,8,9,10]])
+Y = np.array([[10,20,30,40,50,60,70,80,90,100]])
 
 obj = LinearRegression()
 obj.fit(X, Y)
-obj.predict([[3]])
+obj.predict([[8]])
 
             
             

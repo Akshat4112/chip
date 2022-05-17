e9dc40fbfad9154e2f750fa7ade57fae46611aff
Akshat <akshat41121995@gmail.com>
2022-05-17 18:30:39 +0200

    LR Code

--- code/Regression/LinearRegression.py
+++ code/Regression/LinearRegression.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append('../')
+
 import numpy as np
 from Internals.GradientDescent import GradientDescent
 from Internals.Cost import Cost
@@ -9,23 +13,26 @@ class LinearRegression:
         self.std = []
         pass
     
-    def normalize(self, data):
-        for i in range(1,data.shape[1]):
-            data[:,i] = ((data[:,i] - np.mean(data[:,i]))/np.std(data[:, i]))
-            self.mu.append(np.mean(data[:,i]))
-            self.std.append(np.std(data[:, i]))     
-
-    def fit(self, X,y):
+    def fit(self, X,Y):
 
         self.X = X
-        self.Y = y
+        self.Y = Y
+
+        self.Y = np.reshape(self.Y, (4,1))
+        self.X = np.hstack((np.ones((self.X.shape[0],1)), self.X))
+
+
+        print("Input X Shape: ", self.X.shape)
+        print("Input Y Shape: ", self.Y.shape)
+        
         theta = np.zeros((self.X.shape[1],1))
+        print("Theta Shape: ",theta.shape)
 
         def hypothesis(X, theta):
                 return np.matmul(X,theta)
             
         def cost(x,y, theta):
-            return ((hypothesis(x,theta) - y).T@(hypothesis(x,theta)-y)/(2*y.shape[0])) 
+            return ((hypothesis(x,theta) - y).T@(hypothesis(x,theta)-y))/(2*y.shape[0]) 
         
         def gradient_descent(x,y, theta, learning_rate = 0.1, num_epocs = 10):
             m = x.shape[0]
@@ -33,20 +40,28 @@ class LinearRegression:
 
             for _ in range(num_epocs):
                 h_x = hypothesis(x, theta)
+                print(x.T.shape)
+                print(h_x.shape)
                 cost_ = (1/m)*(x.T@(h_x - y))
                 theta = theta - (learning_rate)*cost_
                 J_all.append(cost(x, y, theta))
             
             return theta, J_all
         
-        theta, J_all = gradient_descent(self.X, self.y, self.theta,learning_rate = 0.01, num_epocs = 500)
+        theta, J_all = gradient_descent(self.X, self.Y, theta, learning_rate = 0.01, num_epocs = 500)
         self.beta = theta
+        print("Theta Shape After GD", theta.shape)
 
     def predict(self, X,y):
         return None
     
         
-    
+X = np.array([[1,2,3,4]])
+Y = np.array([[5,6,7,5]])
+
+obj = LinearRegression()
+obj.fit(X, Y)
+
             
             
         
